135efaa178f4d3a8e46424a38b81a5ce4ee68283
Areum Jo <areumjo1@gmail.com>
2019-06-12 20:11:05 -0400

    Get better idea on functions and add comments

--- assignments/prototypes-stretch.js
+++ assignments/prototypes-stretch.js
@@ -1,6 +1,8 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
@@ -15,13 +17,13 @@
 */
 
 function GameObject(att) {
-  this.createdAt = att.createdAt;
-  this.name = att.name;
-  this.dimensions = att.dimensions;
+    this.createdAt = att.createdAt;
+    this.name = att.name;
+    this.dimensions = att.dimensions;
 }
 
 GameObject.prototype.destroy = function() {
-  console.log(`${this.name} was removed from the game`);
+    console.log(`${this.name} was removed from the game`);
 }
 
 /*
@@ -32,16 +34,32 @@ GameObject.prototype.destroy = function() {
 */
 
 function CharacterStats(att) {
-  GameObject.call(this, att);
-  this.healthPoints = att.healthPoints;
-  this.weapons = att.weapons;
+    GameObject.call(this, att);
+    this.healthPoints = att.healthPoints;
+    this.weapons = att.weapons;
 }
 
 CharacterStats.prototype = Object.create(GameObject.prototype);
 
-CharacterStats.prototype.takeDamage = function() {
-  console.log(`${this.name} took damage`);
+CharacterStats.prototype.healthCheck = function() {
+    if (this.healthPoints <= 0) {
+        return this.destroy();
+    }
 }
+
+Object.defineProperty(CharacterStats.prototype, 'healthPointGetterSetter', {
+    get: function() { return this.healthPoints; },
+    set: function(changePoints) { 
+        this.healthPoints += changePoints;
+        if (changePoints > 0) {
+            console.log(`${this.name} recovered damage`);
+        } else {
+        console.log(`${this.name} took damage`);
+        }
+        console.log(`${this.name} has ${this.healthPoints} health points left`);
+        this.healthCheck();
+    }
+});
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -133,62 +151,46 @@ Humanoid.prototype.greet = function() {
   console.log(mage.weapons); // Staff of Shamalama
   console.log(archer.language); // Elvish
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  function Villain(att) {
+function Villain(att) {
     Humanoid.call(this, att);
     this.attack = att.attack;
     this.defend = att.defend;
-  }
-  Villain.prototype = Object.create(Humanoid.prototype);
+}
+Villain.prototype = Object.create(Humanoid.prototype);
   
   
-  function Hero(att) {
+function Hero(att) {
     Humanoid.call(this, att);
     this.attack = att.attack;
     this.defend = att.defend;
-  }
-  Hero.prototype = Object.create(Humanoid.prototype);
+}
+Hero.prototype = Object.create(Humanoid.prototype);
   
   
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  Villain.prototype.dracarys = function(other) {
-    other.healthPoints = other.healthPoints-30;
-    console.log(`${this.name} attacked ${other.name}.\n ${other.name} has ${other.healthPoints} health points left`);
-    if (other.healthPoints <= 0) {
-      return other.destroy();
-    }
-  }
-  Villain.prototype.healthCheck = function() {
-    if (this.healthPoints <= 0) {
-      return targaryen.destroy();
-    }
-  }
+Villain.prototype.dracarys = function(other) {
+    other.healthPointGetterSetter = -20;
+}
   
-  Hero.prototype.nightWatch = function(other) {
-      other.healthPoints = other.healthPoints-20;
-      console.log(`${this.name} attacked ${other.name}.\n ${other.name} has ${other.healthPoints} health points left`);
-  }
-  Hero.prototype.healthCheck = function() {
-    if (this.healthPoints <= 0) {
-      return stark.destroy();
-    }
-  }
+Hero.prototype.nightWatch = function(other) {
+    other.healthPointGetterSetter = -10;
+}
   
   
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
-  const stark = new Hero({
+const stark = new Hero({
     createdAt: new Date(),
     dimensions: {
       length: 6,
       width: 2.5,
       height: 180,
     },
-    healthPoints: 60,
+    healthPoints: 50,
     name: 'Jon Snow',
     team: 'Winter is comming',
     weapons: [
@@ -199,8 +201,8 @@ Humanoid.prototype.greet = function() {
     language: 'northern',
     attack: 10,
     defend: 15
-  });
-  const targaryen = new Villain({
+});
+const targaryen = new Villain({
     createdAt: new Date(),
     dimensions: {
       length: 5.5,
@@ -218,17 +220,12 @@ Humanoid.prototype.greet = function() {
     language: 'dothraki',
     attack: 25,
     defend: 1
-  });
+});
 
 // call functions
 targaryen.dracarys(stark);
+
 stark.nightWatch(targaryen);
 stark.nightWatch(targaryen);
-stark.nightWatch(targaryen);
+stark.nightWatch(stark);
 targaryen.dracarys(stark);
-
-
-targaryen.healthCheck();
-
-
-stark.healthCheck();
